981f56770c855c61052aa42292f8576bd9833677
MortyUganda <114567024+MortyUganda@users.noreply.github.com>
2024-09-03 01:24:56 +0300

    хуета

---
+++ Func unique().py
@@ -0,0 +1,7 @@
+# Implement a generator function that takes a single argument:
+
+# iterable - iterable object
+# The function must return a generator that generates a sequence of elements of the iterable object without duplicates.
+
+def unique(iterable):
+    ...
