43cd7ce265fd239d69488fd3b75561f86193cf90
MortyUganda <114567024+MortyUganda@users.noreply.github.com>
2024-09-03 23:44:11 +0300

    Проверка

---
+++ Func tabulate().py
@@ -0,0 +1,9 @@
+# Implement the tabulate() function, which takes one argument:
+
+# func is an arbitrary function
+# The tabulate() function should return an iterator that 
+# generates an infinite sequence of return values of the func 
+# function, first with argument 1, then 2, then 3, and so on.
+
+def tabulate():
+    ...
