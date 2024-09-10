b48e7bb4e0de0555a1b24261a8fa1bb8b68e1483
MortyUganda <114567024+MortyUganda@users.noreply.github.com>
2024-09-10 08:50:24 +0300

    Initial commit

---
+++ named Students tuple.py
@@ -0,0 +1,23 @@
+# A named Student tuple is available to you, which 
+# contains data about the student. The first element of 
+# the named tuple is the student's last name, the 
+# second is the first name, and the third is the class. A
+#  list of students containing these tuples is also available.
+
+from collections import namedtuple
+from collections import Counter
+
+Student = namedtuple('Student', ['surname', 'name', 'grade'])
+
+students = [Student('Гагиев', 'Александр', 10), Student('Дедегкаев', 'Илья', 11), Student('Кодзаев', 'Георгий', 10),
+            Student('Набокова', 'Алиса', 11), Student('Кораев', 'Артур', 10), Student('Шилин', 'Александр', 11),
+            Student('Уртаева', 'Илина', 11), Student('Салбиев', 'Максим', 10), Student('Капустин', 'Илья', 11),
+            Student('Гудцев', 'Таймураз', 11), Student('Перчиков', 'Максим', 10), Student('Чен', 'Илья', 11),
+            Student('Елькина', 'Мария', 11),Student('Макоев', 'Руслан', 11), Student('Албегов', 'Хетаг', 11),
+            Student('Щербак', 'Илья', 10), Student('Идрисов', 'Баграт', 11), Student('Гапбаев', 'Герман', 10),
+            Student('Цивинская', 'Анна', 10), Student('Туткевич', 'Юрий', 11), Student('Мусиков', 'Андраник', 11),
+            Student('Гадзиев', 'Георгий', 11), Student('Белов', 'Юрий', 11), Student('Акоева', 'Диана', 11),
+            Student('Денисов', 'Илья', 11), Student('Букулова', 'Диана', 10), Student('Акоева', 'Лера', 11)]
+
+dct = Counter([k.name for k in students])
+print(max(dct, key=lambda x: dct[x]))
