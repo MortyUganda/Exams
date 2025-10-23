7efeb0f832fb23e804b78a5bb7605eb433104dcd
MortyUganda <kurganskiy.1990@mail.ru>
2025-10-23 18:33:58 +0300

    Initial commit

---
+++ RE_Одинаковые слоги.py
@@ -0,0 +1,29 @@
+# Write a program that prints words consisting of two identical parts.
+
+#  Input format
+# The program is given an arbitrary number of words, each on a separate line.
+
+#  Output format
+# The program should print only those words from the input that consist of two identical syllables. 
+# The words should be arranged in their original order, each on a separate line.
+
+import re
+import sys
+
+pattern = r'(\w+)\1'
+
+for el in sys.stdin:
+    res = re.match(pattern, el)
+    if res:
+        print(res.group)
+
+# Sample Input 1:
+
+# Python
+# beebee
+# PyPy
+# portal
+# Sample Output 1:
+
+# beebee
+# PyPy
